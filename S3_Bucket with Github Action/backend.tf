e2568c12db2360a067e4fa8ca7032f17c123fc16
YUVRAJ KUMAR ARORA <yuvrajarora070@gmail.com>
2023-06-09 20:36:25 +0530

    updated Code

--- S3_Bucket with Github Action/backend.tf
+++ S3_Bucket with Github Action/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "mybucket-1410"
-        key = "mayank/mayank-tfstate"
+        key = "yuvraj/yuvraj-tfstate"
         region = "us-east-1"
     }
 }
